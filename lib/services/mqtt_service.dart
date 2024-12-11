c5ff202e402dce6b7c9c3063d2ec3f461d241314
Painapple <105093357+Pain4pple@users.noreply.github.com>
2024-12-11 10:49:29 +0800

    Update mqtt_service.dart

--- lib/services/mqtt_service.dart
+++ lib/services/mqtt_service.dart
@@ -221,13 +221,13 @@ class MQTTClientWrapper {
     client.publishMessage(topic, MqttQos.exactlyOnce, builder.payload!, retain: retainBool);
   }
 
-    void publishMessage(String topic, String message, bool retain) {
-    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
-    builder.addString(message);
+  //   void publishMessage(String topic, String message, bool retain) {
+  //   final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+  //   builder.addString(message);
 
-    print('Publishing message "$message" to topic $topic');
-    client.publishMessage(topic, MqttQos.exactlyOnce, builder.payload!, retain: retain);
-  }
+  //   print('Publishing message "$message" to topic $topic');
+  //   client.publishMessage(topic, MqttQos.exactlyOnce, builder.payload!, retain: retain);
+  // }
 
   void _onSubscribed(String topic) {
     print('Subscription confirmed for topic $topic');
